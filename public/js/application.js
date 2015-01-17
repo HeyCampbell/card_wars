b3bf81cc4c859c31d0afa6f7cfadacc88eee92c8
Jon Campbell <heycampbell@gmail.com>
2015-01-17 17:41:41 -0500

    added css and js

--- public/js/application.js
+++ public/js/application.js
@@ -2,6 +2,56 @@ $(document).ready(function() {
   // This is called after the document has loaded in its entirety
   // This guarantees that any elements we bind to will exist on the page
   // when we try to bind to them
+  $('#flip_button').on('click', function() {
+    event.preventDefault()
+    $.ajax({
+      // send data to server, could be nothing
+      url: '/game',
+      type: 'GET'
+    }).done(function(response) {
+      // do stuff with the data that server return
+      console.log("HI")
+      console.log(response)
+    })
+  })
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
 });
+
+
+
+//   var score = function() {
+//     var war_booty = []
+//     if card1.value >
+//      var winner = session[:hand1]
+//       winner << session[:hand2].shift
+//     else if card1.value < card2.value
+//      winner = session[:hand2]
+//      winner << session[:hand1].shift
+//     else if card1.value == card2.value
+//      war_booty << session[:hand1].shift(3)
+//      war_booty << session[:hand2].shift(3)
+//      score()
+//    end
+// });
+//   var score = {
+//     checkScore : function () {
+//         alert('hello');
+//     }
+// }
+
+
+
+// # def score(card1 = Card.find(session[:hand1][0], card2 = Card.find(session[:hand2][0]))
+// #   war_booty = []
+// #
+// #   winner << war_booty
+// #   winner.flatten.rotate!
+// # end
+
+// # def winner?
+// #   if session[:hand1].length == 0
+// #     session[:winner] = session[:player2]
+// #   elsif session[:hand2].length == 0
+// #     session[:winner] = session[:player1]
+// #   end
+// # end
